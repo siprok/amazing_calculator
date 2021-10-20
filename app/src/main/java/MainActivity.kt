ec4f796a44146859d60fa90776262ceab49d4163
siprok <siprokudin@gmail.com>
2021-10-20 19:18:47 +0300

    first version of layout completed

---
+++ app/src/main/java/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.amazing_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity()
+{
+
+}
--- app/src/test/java/calc_logic.kt
+++ app/src/test/java/calc_logic.kt
@@ -39,15 +39,23 @@ fun evaluatedFormatter(evaluated: String): String =
             else -> "Нерассмотренная ошибка"
         }
     }
-fun main() {
+
+fun calc_test()
+{
     val testStrings = arrayOf("5/0",
-        "((5+2)",
-        "sqrt(1+3)",
-        "√(1+3)",
-        "(5+2)(1+3)",
-        "2^3",
-        "4^0.5",
-        "4^(0-0.5)",
-        "4^(-0.5)")
+    "((5+2)",
+    "sqrt(1+3)",
+    "√(1+3)",
+    "(5+2)(1+3)",
+    "2^3",
+    "4^0.5",
+    "4^(0-0.5)",
+    "4^(-0.5)",
+    "sqrt(-1)")
     testStrings.forEach { println("Input $it \nOutput ${evaluatedFormatter(evaluate(it))}") }
+}
+
+fun main()
+{
+    calc_test();
 }
